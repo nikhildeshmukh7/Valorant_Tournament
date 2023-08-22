fdd5904e98e255dd46f5c9b12488ed76e02448c1
Nikhil <Nikhil@LAPTOP-KT8NUG04>
2023-08-22 23:07:33 +0530

    f ve

---
+++ Valorant_Tournament/src/main/java/com/valo/ValorantTournamentApplication.java
@@ -0,0 +1,13 @@
+package com.valo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorantTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValorantTournamentApplication.class, args);
+	}
+
+}
---
+++ Valorant_Tournament/src/test/java/com/valo/ValorantTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValorantTournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
